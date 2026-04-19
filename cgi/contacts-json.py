492b5c2cf02b4de976b0e69bf20f7d755de6f421
Bui Thi Hong Ngoc[ Aylin ][ Project Management ] <ngoc.bui@navercorp.com>
2026-04-19 15:55:36 +0700

    add email + css

--- cgi/contacts-json.py
+++ cgi/contacts-json.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import pymysql
 import json
@@ -6,46 +6,27 @@ import json
 print("Content-Type: application/json")
 print()
 
-response = {
+conn = pymysql.connect(
+    host="localhost",
+    user="testuser",
+    password="1234",
+    database="contacts_db"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT name, phone, email FROM contacts")
+
+data = []
+for name, phone, email in cursor.fetchall():
+    data.append({
+        "name": name,
+        "telephone": phone,
+        "email": email
+    })
+
+print(json.dumps({
     "ok": True,
-    "count": 0,
-    "data": []
-}
+    "data": data
+}))
 
-try:
-    conn = pymysql.connect(
-        host="localhost",
-        user="testuser",
-        password="1234",
-        database="contacts_db",
-        charset="utf8"
-    )
-
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, phone, email FROM contacts")
-    for name, phone, email in cursor.fetchall():
-        response["data"].append({
-             "name": name,
-            "telephone": phone,  
-            "email": email
-             })
-             
-    rows = cursor.fetchall()
-
-    for name, phone in rows:
-        response["data"].append({
-            "name": name,
-            "telephone": phone
-        })
-
-    response["count"] = len(response["data"])
-
-    conn.close()
-
-except Exception as e:
-    response = {
-        "ok": False,
-        "error": str(e)
-    }
-
-print(json.dumps(response, ensure_ascii=False))
+conn.close()
